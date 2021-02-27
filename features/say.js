2887bdb1ff1fe45fb7efd79589fedc9e80be3724
Alexander Viala Bellander <alexander.viala.bellander@ivao.aero>
2021-02-27 19:55:47 +0100

    Initial commit

---
+++ features/say.js
@@ -0,0 +1,96 @@
+let command = {
+    name: "say",
+        description: "interact as Hacke",
+        options: [
+            {
+                name: "create",
+                description: "create a post as Hacke",
+                type: 1,
+                options: [
+                    {
+                        name: "title",
+                        description: "title of the message",
+                        type: 3,
+                        required: true
+                    },
+                    {
+                        name: "description",
+                        description: "explanatory message that will be posted in the channel",
+                        type: 3,
+                        required: true
+                    },
+                    {
+                        name: "channel",
+                        description: "choose a channel to post your message",
+                        type: 7,
+                        required: false
+                    }
+                ]
+            },
+            {
+                name: "edit",
+                description: "edit a Hacke message",
+                type: 1,
+                options: [
+                    {
+                        name: "title",
+                        description: "title of the message",
+                        type: 3,
+                        required: false
+                    },
+                    {
+                        name: "description",
+                        description: "explanatory message that will be posted in the channel",
+                        type: 3,
+                        required: false
+                    },
+                    {
+                        name: "message-id",
+                        description: "send the message ID you want to edit",
+                        type: 3,
+                        required: false
+                    }
+                ]
+            } 
+        ]
+}
+
+
+
+async function handle(Discord, client, interaction, command, args) {
+    if(args[0]["name"] == "create") {
+        const title = args[0]["options"].find(arg => arg.name.toLowerCase() == "title").value
+        const description = args[0]["options"].find(arg => arg.name.toLowerCase() == "description").value
+        var channel = "default"
+        try {
+            channel = args[0]["options"].find(arg => arg.name.toLowerCase() == "channel").value
+        } catch {
+        }
+        const embedMessage = new Discord.MessageEmbed()
+            .setTitle(title)
+            .setDescription(description)
+            .setColor(0xfa6607)
+        client.channels.cache.get(channel == "default" ? interaction.channel_id : channel).send("", embedMessage)
+
+    } else edit : if (args[0]["name"] == "edit"){
+        const title = args[0]["options"].find(arg => arg.name.toLowerCase() == "title").value
+        const description = args[0]["options"].find(arg => arg.name.toLowerCase() == "description").value
+        const target = args[0]["options"].find(arg => arg.name.toLowerCase() == "message-id").value
+        let targetChannel = await client.channels.fetch(interaction.channel_id)
+        try {
+            let message = await targetChannel['messages'].fetch(target)
+        } catch {
+            console.log("HACKE.EDIT : Message not found in interaction channel")
+            break edit
+        }
+        const embedMessage = new Discord.MessageEmbed()
+            .setTitle(title)
+            .setDescription(description)
+            .setColor(0xfa6607)
+        console.log()
+        message.edit("", embedMessage)
+    }
+}
+
+exports.handle = handle;
+exports.command = command;
---
+++ main.js
@@ -0,0 +1,51 @@
+const Discord = require("discord.js")
+const say = require("./features/say.js")
+const client = new Discord.Client()
+
+client.on("message", message => {
+    if (message.content === "ping") {
+      message.channel.send("pong");
+    }
+})
+
+client.once("ready", () => {
+    console.log("bot is online")
+    /*
+    client.api.applications(client.user.id).guilds('812278673386111016').commands.get().then(function(data) {
+        console.log(data);
+    });
+    
+
+    
+    client.api.applications(client.user.id).guilds('812278673386111016').commands('815248387271426099').delete().then(function(data) {
+        console.log(data);
+    });
+
+    client.api.applications(client.user.id).guilds('812278673386111016').commands.get().then(function(data) {
+        console.log(data);
+    }); 
+    */
+
+    client.api.applications(client.user.id).guilds('812278673386111016').commands.post({
+        data: say.command
+    })
+
+    client.ws.on("INTERACTION_CREATE", async interaction => {
+        const command = interaction.data.name.toLowerCase()
+        const args = interaction.data.options
+
+        if(command == "say") {
+            say.handle(Discord, client, interaction, command, args)
+        }
+    })
+    
+})
+
+async function createAPIMessage(interaction, content) {
+    const apiMessage = await Discord.APIMessage.create(client.channels.resolve(interaction.channel.id), content)
+        .resolveData()
+        .resolveFiles()
+    return apiMessage
+}
+
+client.login('ODEyMzE0Mzg0NTYxODY0NzM2.YC-8uQ.FTV45fE67raOydDhkMGd1TsRds8')

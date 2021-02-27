3649f12d8642e6db7c29ab2e37392d3fab367b17
Alexander Viala Bellander <grubsevb@gmail.com>
2021-02-27 20:30:55 +0100

    Main.js Cleanup

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js")
 const say = require("./features/say.js")
-const config = require("config.json")
+const react = require("./features/react.js")
+const config = require("./config.json")
 
 
 const client = new Discord.Client()
@@ -13,7 +14,7 @@ client.on("message", message => {
 
 client.once("ready", () => {
     console.log("bot is online")
-    console.log(config)
+
     /*
     client.api.applications(client.user.id).guilds('812278673386111016').commands.get().then(function(data) {
         console.log(data);
@@ -30,26 +31,36 @@ client.once("ready", () => {
     }); 
     */
 
+    //https://discord.com/developers/docs/interactions/slash-commands#create-guild-application-command
     client.api.applications(client.user.id).guilds('812278673386111016').commands.post({
         data: say.command
     })
 
+    client.api.applications(client.user.id).guilds('812278673386111016').commands.post({
+        data: react.command
+    })
+
+    //https://discord.com/developers/docs/interactions/slash-commands#interaction
     client.ws.on("INTERACTION_CREATE", async interaction => {
         const command = interaction.data.name.toLowerCase()
         const args = interaction.data.options
 
+        switch(command) {
+            case "say":
+                say.handle(Discord, client, interaction, command, args)
+                break;
+            case "react":
+                console.log("rolebot command launched")
+              break;
+            default:
+              // code block
+          }
+
+
         if(command == "say") {
-            say.handle(Discord, client, interaction, command, args)
         }
     })
     
 })
-
-async function createAPIMessage(interaction, content) {
-    const apiMessage = await Discord.APIMessage.create(client.channels.resolve(interaction.channel.id), content)
-        .resolveData()
-        .resolveFiles()
-    return apiMessage
-}
 
 client.login(config)
